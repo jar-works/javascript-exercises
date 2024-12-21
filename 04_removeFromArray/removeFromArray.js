579472d47ebd477c0df6369b9287656126683a38
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-12-21 17:39:14 -0500

    feat: Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    // Get all arguments after the first one (if multiple)
+    let removeArr = Array.from(arguments).slice(1);
+
+    return arr.filter(value => !removeArr.includes(value));
 };
 
 // Do not edit below this line
