a77f2681e428c68de403af2b023a063e479046d7
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-12-21 17:01:30 -0500

    feat: Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (!num) { // No repeat
+        return "";
+    } else if (num < 0) {
+        return "ERROR";
+    }
 
+    let originalString = string;
+
+    for (let i = 0; i < num - 1; i++) {
+        string += originalString;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
