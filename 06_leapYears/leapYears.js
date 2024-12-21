2c0c626befe465b84757a2f57573ca2853ede826
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-12-21 18:21:20 -0500

    docs: Add note explaining better readability

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,3 +1,6 @@
+// Could be made a lot more readable, such as the solution, by using variables
+// and having good naming conventions
+
 const leapYears = function(year) {
     if (year % 100 == 0 && year % 400 != 0) {
         return false;
