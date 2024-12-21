a722650a819d2531764980bf750b1a9d231ad9b5
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-12-21 18:06:33 -0500

    feat: Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
 
+    let result = 0;
+
+    let start = num1
+    let finish = num2;
+
+    if (num1 > num2) {
+        start = num2;
+        finish = num1;
+    }
+
+    for (let i = start; i <= finish; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
