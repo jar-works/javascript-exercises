e37fb24fd71c9764728ce17ad94973fae8a31263
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2025-01-02 14:28:47 -0500

    feat: Complete exercise 10 - fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof(num) == "string") {
+        num = parseInt(num);
+    }
+    
+    if (num === 0) { 
+        return 0; 
+    } else if (num < 0) { 
+        return "OOPS"; 
+    }
+    
+    let lastResult = 0;
+    let currentFib = 1;
 
+    for (let i = 1; i <= num; i++) {
+        if (num === i) { return currentFib; }
+        const temp = currentFib;
+        currentFib += lastResult;
+        lastResult = temp;
+    }
 };
 
 // Do not edit below this line
