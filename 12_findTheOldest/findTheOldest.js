71a379d4bf481dc065348b89a79279a757965402
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2025-01-02 15:01:59 -0500

    feat: Complete exercise 12 - findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people = []) {
+    const newArr = people.sort((firstPerson, secondPerson) => {
 
+        if (!firstPerson["yearOfDeath"]) {
+            firstPerson["yearOfDeath"] = new Date().getFullYear();
+        } else if (!secondPerson["yearOfDeath"]) {
+            secondPerson["yearOfDeath"] = new Date().getFullYear();
+        }
+        const firstAge = firstPerson.yearOfDeath - firstPerson.yearOfBirth;
+        const secondAge = secondPerson.yearOfDeath - secondPerson.yearOfBirth;
+
+        return secondAge - firstAge;
+    }).filter(person => person["yearOfDeath"]);
+
+    console.table(newArr);
+    return newArr[0];
 };
 
 // Do not edit below this line
