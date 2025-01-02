31413c9f26bd3212ed98ac4e92cb4c7171c13d6e
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2025-01-02 14:02:14 -0500

    feat: Complete exercise 09 - palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str = "") {
+    const cleanStr = str.split("").filter(isCharOrNumber).join("").toLocaleLowerCase();
+    const reversedStr = cleanStr.split("").reverse().join("").toLocaleLowerCase();
+    console.log(`${cleanStr} ${reversedStr}`);
+    return reversedStr === cleanStr;
 };
+
+// Will only return true if the char being checked is a number or char
+function isCharOrNumber(char) {
+    const result = /[a-zA-Z]/.test(char) || /[0-9]/.test(char);
+    console.log(result);
+    return result;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
